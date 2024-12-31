f8689827f07a34a1c6f2417b2b7cab4d7f0e2c13
Rajat Chakraborty <rajatc2@illinois.edu>
2024-12-30 18:59:38 -0600

    Add files via upload
    # Added comments to serial_ion_for_dwell_time.tcl for better readability

--- serial_ion_for_dwell_time.tcl
+++ serial_ion_for_dwell_time.tcl
@@ -1,13 +1,13 @@
 # Put the detail here
-set sprefix nw_mosse_se-se_d15.8_0.8nm_ions
-set dcd nw_mosse_se-se_d15.8_0.8nm_20.0V_1 
+set sprefix #put the name of the psf file here (without .psf)
+set dcd #put the name of the psf file here (without .dcd)
 set final_frame 5400
 set zmax 7.9
 set zmin -7.9
-set ip ./
-set op ./
+set ip ./		#input folder directory
+set op ./		#input folder directory
 
-source ion_calculator_modified.tcl
+source dwell_time_calculator.tcl
 
 ion_translocate $sprefix $dcd POT $final_frame $zmax $zmin $ip $op
 ion_translocate $sprefix $dcd CLA $final_frame $zmax $zmin $ip $op
